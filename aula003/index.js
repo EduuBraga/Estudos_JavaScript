6eb9209960b7f9790a6a966b5f67a6dbaf1dc4df
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-10-19 15:16:42 -0300

    Chore: Exercício de arrays

---
+++ aula003/index.js
@@ -0,0 +1,24 @@
+// Arrays
+let heroes = ["Super Man", "Batman", "Thor"]
+console.log(heroes)
+
+// indexOF
+let thorIndex = heroes.indexOf('Thor')
+console.log(heroes[thorIndex])
+
+// Join
+let heroesJoin = heroes.join(' | ')
+console.log(heroesJoin)
+
+// Concat
+let heroesConcant = heroes.concat(['Aquaman', 'Capitan america'])
+console.log(heroesConcant)
+
+// Push
+let pushHeroes = heroes.push('Aquaman', 'Soldier Boy')
+console.log(heroes)
+
+// Pop
+let popHeroes = heroes.pop()
+console.log(heroes)
+console.log(popHeroes)
