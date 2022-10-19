cfd21c6192a8899cf69f8e32a4f10b7f1b2ddd19
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-10-19 11:03:05 -0300

    Chore: Exercício de números

--- aula002/index.js
+++ aula002/index.js
@@ -1,1 +1,24 @@
 // Numbers
+let radio = 10
+let pi = 3.14
+
+const crazyOperation = 5 * (3 - 10) ** 2
+
+console.log(crazyOperation)
+
+//Incremento e decremento
+let postLikes = 10
+postLikes--
+postLikes++
+
+console.log(postLikes)
+
+// Operations assignments
+
+postLikes /= 2
+postLikes += 10
+postLikes -= 10
+postLikes *= 10
+
+console.log(postLikes)
+
