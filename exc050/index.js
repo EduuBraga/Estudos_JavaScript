7b43f5122b6caf3567447141c7d07d93c3262d9e
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-02 20:59:45 -0300

    Feat:Implemetando função de callback na requisição

--- exc050/index.js
+++ exc050/index.js
@@ -1,10 +1,29 @@
-const request = new XMLHttpRequest()
+const getTodos = callback => {
+  const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', () => {
-  if(request.readyState === 4 && request.status === 200){
-    console.log(request.responseText)
+  request.addEventListener('readystatechange', () => {
+    const isRequestOk = request.readyState === 4 && request.status === 200
+    const isRequestNotOk = request.readyState === 4
+
+    if (isRequestOk) {
+      callback(null, request.responseText)
+      return
+    }
+
+    if (isRequestNotOk) {
+      callback('Requisição mal sucedida.', null)
+    }
+  })
+
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
+  request.send()
+}
+
+getTodos((error, data) => {
+  if(error){
+    console.log(error)
+    return
   }
-})
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
+  console.log(data)
+})
