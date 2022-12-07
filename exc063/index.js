c9821b8e5092b5ccae119e5b22ec6b185eaf1e7b
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-07 16:27:26 -0300

    Usando o try/catch em erros de requisições

---
+++ exc063/index.js
@@ -0,0 +1,20 @@
+const getUsers = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users') 
+
+    if(!response.ok){
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    return response.json()
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+const logUsers = async () => {
+  const users = await getUsers()
+  console.log(users)
+}
+
+logUsers()
