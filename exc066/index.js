5c94abad252ea4103ec25d994d51b20d7be17767
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-13 15:01:09 -0300

    Manipulando JSON em conjunto com o LocalStorage

--- exc066/index.js
+++ exc066/index.js
@@ -1,9 +1,19 @@
 localStorage.setItem('name', 'Eduardo')
 localStorage.setItem('age', 19)
 
-localStorage.clear()
-
 let name = localStorage.getItem('name')
 let age = localStorage.getItem('age')
 
 console.log(name, age)
+
+const myArray = [
+  { a: 1, b: 2 },
+  { c: 3, d: 4 },
+  { e: 5, f: 5 }
+]
+
+localStorage.setItem('myArray', JSON.stringify(myArray))
+const JSONToLocalStorage = localStorage.getItem('myArray')
+
+console.log(JSONToLocalStorage)
+console.log(JSON.parse(JSONToLocalStorage))
