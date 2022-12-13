62b2f901ff08b3bdee7495df8d7f41d466a6c1f7
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-13 17:16:27 -0300

    Finalizei desafios de localStorage e lógica

---
+++ exc067/index.js
@@ -0,0 +1,193 @@
+/*
+  01
+
+  - Implemente uma função que recebe o nome da key de um item da localStorage 
+    e retorna o valor da key parseado para objeto javascript.
+*/
+const myObjJSON = '{"name": "eduardo", "age": 18}'
+
+localStorage.setItem('i', myObjJSON)
+
+const valueParsing = key => JSON.parse(localStorage.getItem(key))
+
+console.log(valueParsing('i'))
+
+/*
+  02
+
+  - Mesmo com um input type="number", a expressão `event.target.value` abaixo 
+    resulta em uma string;
+  - Exiba, no console, o valor que foi inserido no input;
+  - O exibido valor deve ser do tipo number;
+  - Não utilize a invocação da Number() ou qualquer outro método que converta 
+    strings em number.
+
+  Dica: pesquise por valueAsNumber.
+*/
+
+const input = document.querySelector('[data-js="input"]')
+
+input.addEventListener('input', event => {
+  const number = new Number(event.target.value)
+  console.log(number.valueOf())
+})
+
+/*
+  03
+
+  - Implemente uma função 'combineOperations' que recebe 2 parâmetros:
+    - Um valor inicial, do tipo number;
+    - Um array de funções.
+  - A combineOperations deve: 
+    - Passar o valor inicial para a 1ª função do array;
+    - Passar o valor retornado pela invocação da 1ª função para a 2ª função, e 
+      assim por diante. Até que cada função do array tenha sido invocada;
+    - Retornar o valor que a invocação da última função do array retornou.
+  - Descomente o código abaixo. A primeira invocação da combineOperations deve 
+    retornar 60 e a segunda invocação, 10.
+*/
+const add100 = num => num + 100
+
+const divByFive = num => num / 5
+
+const multiplyByThree = num => num * 3
+
+const multiplyFive = num => num * 5
+
+const addTen = num => num + 10
+
+const combineOperations = (initialValue, functions) => {
+  let value = initialValue
+
+  functions.forEach(func => {
+    value = func(value)
+  });
+
+  return value
+}
+
+const combineOperationsTwo = (initialValue, functions) => functions
+  .reduce((acc, func) => func(acc), initialValue)
+
+console.log(combineOperations(0, [add100, divByFive, multiplyByThree]))
+console.log(combineOperationsTwo(0, [divByFive, multiplyFive, addTen]))
+
+/*
+  04
+
+  - O código abaixo não está funcionando. Descubra o que ele está tentando 
+    fazer e refatore-o.
+*/
+
+const albums = [
+  {
+    id: 537,
+    title: 'The Dark Side of the Moon',
+    artist: 'Pink Floyd',
+    price: 59.90,
+    genre: 'Progressive Rock'
+  },
+  {
+    id: 975,
+    title: 'Houses of the Holy',
+    artist: 'Led Zeppelin',
+    price: 81.00,
+    genre: 'Rock'
+  },
+  {
+    id: 359,
+    title: 'Heaven and Hell',
+    artist: 'Black Sabbath',
+    price: 49.90,
+    genre: 'Heavy metal'
+  }
+]
+
+const albumHousesOfTheHoly = {
+  id: 975,
+  title: 'Houses of the Holy',
+  artist: 'Led Zeppelin',
+  price: 81.00,
+  genre: 'Rock'
+}
+
+const searchAlbum = albumParam => {
+  const albumExist = albums.find(album => album.id === albumParam.id)
+
+  if (albumExist){
+    console.log(`${JSON.stringify(albumParam)} existe no array albums.`)
+    return
+  }
+
+  console.log('Album não existe no array.')
+}
+
+searchAlbum(albumHousesOfTheHoly)
+searchAlbum({ id: 1 })
+
+/*
+  05
+
+  - Baseado no que foi mostrado na aula passada, faça uma cópia do `obj`.
+*/
+
+const obj = {
+  prop1: 'a',
+  prop2: 'b',
+  prop3: null,
+  prop4: true,
+  prop5: false,
+  prop6: [9, { x: 1, y: 2 }],
+  prop7: 7,
+  prop8: { a: 'x', b: 'y' },
+}
+
+const obj1 = {...obj}
+
+localStorage.setItem('obj2', JSON.stringify(obj))
+const obj2 = JSON.parse(localStorage.getItem('obj2'))
+
+console.log(obj.prop1 = '1', obj)
+console.log(obj1.prop1 = '2', obj1)
+console.log(obj2.prop1 = '3', obj2)
+
+/*
+  06
+
+  - Implemente uma função que cria e retorna um elemento HTML;
+  - Ela deve receber o nome do elemento HTML a ser criado e um objeto com os 
+    atributos que o elemento deve conter;
+  - A quantidade de atributos que o elemento irá conter pode variar.
+
+  Dica: pesquise por Object.entries.
+*/
+
+const createELementHTML = (tag, attrs, text) => {
+  const arrayAttributes = Object.entries(attrs)
+  let attributes = ''
+
+  arrayAttributes.forEach(attr => {
+    let key = attr[0]
+    let value = attr[1]
+
+    attributes += ` ${key}="${value}"`
+  });
+
+  const element = `<${tag}${attributes}> ${text} </${tag}>`
+
+  return element
+}
+
+const p = createELementHTML('p', {class: 'title', id: 'paragraph'}, 'Olá, mundo!')
+
+document.querySelector('body').innerHTML = p
+
+/*
+  07
+
+  - Na weather app, faça com que quando o usuário recarregar a página ou sair 
+    da aplicação e voltar, as informações da última cidade pesquisada sejam 
+    exibidas na interface.
+*/
+
+
