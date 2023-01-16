503e06a208993322546c44f3cffb6d0182750bef
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-16 16:10:41 -0300

    Métodos em classes

--- exc070/index.js
+++ exc070/index.js
@@ -11,13 +11,26 @@ console.log(str.length)
 
 // Class
 class User {
-  constructor(name, email){
+  constructor(name, email) {
     this.name = name
     this.email = email
+    this.points = 0
+  }
+
+  login() {
+    console.log(`${this.name} logado com sucesso!`)
+    return this
+  }
+
+  addPoint() { 
+    this.points++
+    return `${this.name} está com ${this.points} ${this.points > 1 ? 'pontos' : 'ponto'}`
   }
 }
 
 const user1 = new User('Eduardo', 'dudu@gamil.com.br')
 const user2 = new User('Hellen Vitória', 'hv@gamil.com.br')
 
-console.log(user1, user2)
+user1.login().addPoint()
+
+console.log(user1)
